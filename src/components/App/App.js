6ec396e82e54ee668f93348a0ec3117b51080e27
Chameng Vang <Chameng.vang@outlook.com>
2022-10-02 14:27:37 -0500

    create and add header component.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,13 @@
 import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
-import MovieList from '../MovieList/MovieList'
-import DetailsPage from '../DetailsPage/DetailsPage';
+import MovieList from '../MovieList/MovieList.jsx'
+import DetailsPage from '../DetailsPage/DetailsPage.jsx';
+import Header from '../Header/Header.jsx';
 
 function App() {
   return (
     <div className="App">
-      <h1>The Movies Saga!</h1>
+      <Header />
       <Router>        
         <Route path="/" exact>
           <MovieList />
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+function Header () {
+    return (
+        <div className='headerDiv'>
+            <h1>The Movies Saga!</h1>   
+        </div>
+    )
+}
+
+export default Header;
